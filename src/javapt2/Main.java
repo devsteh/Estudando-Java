d3d45ccce481877a6d7d2f9532cf3c87f6a9085e
Stephany <lifeuniversitary@gmail.com>
2022-07-14 23:40:35 -0300

    Atríbuição de tipos de dados com variáveis

---
+++ src/javapt2/Main.java
@@ -0,0 +1,59 @@
+package javapt2;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Atribuindo tipos de dados junto de variáveis.
+        byte b1 = 10;
+        byte b2 = 20;
+
+        short s1 = 20000;
+        //short s1 = 40000;
+
+        //int i1 = 1000000000000;
+        int i2 = 28500;
+
+        //Para tipo long, é necessário colocar "L" no fim do número.
+        long l1 = 1000000000000000000L;
+        long l2 = 200000000000000000L;
+
+        //Para tipo float, é necessário colocar "f" no fim do número, caso contário o número é interpretado como double.
+        //float f1 = 4.5;
+        float f2 = 10.68f;
+
+        double d1 = 85.69;
+        double d2 = 99.04d;
+
+        char c1 = 'W';
+        //char c2 = 'Tw';
+        char c3 = '\u0057';
+
+        String st1 = "User1";
+        String st2 = "User2";
+        String st3 = "ag dgb se se et t BASBAUSBAUSB &5@$#¨¨";
+
+        //String dt1 = "14/07/2022";
+
+        boolean bo1 = true;
+        boolean bo2 = false;
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(s1);
+        System.out.println(i2);
+        System.out.println(b1);
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(f2);
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(c1);
+        System.out.println(c3);
+        System.out.println(st1);
+        System.out.println(st2);
+        System.out.println(st3);
+        System.out.println(bo1);
+        System.out.println(bo2);
+
+    }
+}
