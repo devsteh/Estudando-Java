89421ed00f1670a9a7513821dd9f6405394391b8
Stephany <lifeuniversitary@gmail.com>
2022-07-14 22:59:07 -0300

    Conceituação, criação de variáveis e atribuição de valores

---
+++ src/javapt1/Main.java
@@ -0,0 +1,52 @@
+package javapt1;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Tipos de criação de variáveis - Linhas comentadas = exemplos de atribuições incorretas.
+        int i;
+        //int i;
+        int I;
+        //int 1a;
+        int _1a;
+        int $aq;
+
+        //Declaração de valores nas variáveis.
+        i = 5;
+        I = 10;
+        _1a = 20;
+        $aq = 7;
+
+        //Tipos de criação de variáveis junto de atribuições de valores - Linhas comentadas = exemplos de atribuições incorretas.
+        final int j = 10;
+        //j = 15;
+        int asrn24678md;
+        //int asrn246 78md;
+        int asrn246$78_md = 10;
+        //int asrn246%78_md = 10;
+
+        asrn24678md = 100;
+        asrn246$78_md = 10;
+
+        int quantidadeProduto = 50;
+        //int QuantidadeProduto;
+        final int NUMERO_TENTATIVAS = 5;
+        //final int numero_Tentativas = 5;
+        int QUANTIDADE_OPCOES = 25;
+        //int qtdProd;
+
+        //Print de variáveis
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println(_1a);
+        System.out.println($aq);
+
+        System.out.println(j);
+        System.out.println(asrn24678md);
+        System.out.println(asrn246$78_md);
+
+        System.out.println(quantidadeProduto);
+        System.out.println(NUMERO_TENTATIVAS);
+        System.out.println(QUANTIDADE_OPCOES);
+    }
+}
